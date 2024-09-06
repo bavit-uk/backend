af814736227fe0fa9efd47b9993da9712ce9e3b0
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2024-09-06 17:11:45 +0500

    chore: Update user contract with additional fields
    The `user.contract.ts` file has been updated to include additional fields in the `UserCreatePayload` type. The `otp`, `mobileNumber`, `countryCode`, and `countryCodeName` fields have been added to the type definition. This change ensures that the `UserCreatePayload` type accurately represents the required fields for creating a user.

--- src/contracts/user.contract.ts
+++ src/contracts/user.contract.ts
@@ -1,14 +1,41 @@
+import { ENUMS } from "@/constants/enum";
 import { Model, ObjectId } from "mongoose";
 
 export interface IUser {
   id: ObjectId;
   name: string;
   email: string;
-  emailVerified: boolean;
-  emailVerifiedAt?: Date;
   password: string;
   passwordResetAt?: Date;
-  role: "user" | "admin";
+  role: (typeof ENUMS.USER_TYPES)[number];
+  dob?: Date;
+  mobileNumber: string;
+  countryCode: string;
+  countryCodeName: string;
+  profilePicture?: string;
+  address?: string;
+  status: (typeof ENUMS.USER_STATUS)[number];
+  notificationStatus: (typeof ENUMS.NOTIFICATION_STATUS)[number];
+  locationStatus: (typeof ENUMS.LOCATION_STATUS)[number];
+  latitude?: number;
+  longitude?: number;
+  otp?: string;
+  otpExpiresAt?: Date;
+  verified: boolean;
+  verifiedAt?: Date;
+  emailVerified: boolean;
+  emailVerifiedAt?: Date;
+  hasTwoFactorAuth: boolean;
+  bio?: string;
+  loginWithQRCode: boolean;
+  loginQRCode?: string;
+  profileQrCode?: string;
+  planId?: ObjectId;
+  isPlanActive: boolean;
+  planPurchasedAt?: Date;
+  planExpiredAt?: Date;
+  deviceType?: (typeof ENUMS.DEVICE_TYPES)[number];
+  deviceUniqueId?: string;
 }
 
 export interface IUserMethods {
@@ -16,7 +43,10 @@ export interface IUserMethods {
   hashPassword: (password: string) => string;
 }
 
-export type UserCreatePayload = Pick<IUser, "name" | "email" | "password" | "role">;
+export type UserCreatePayload = Pick<
+  IUser,
+  "name" | "email" | "password" | "otp" | "mobileNumber" | "countryCode" | "countryCodeName"
+>;
 
 export type UserUpdatePayload = Partial<UserCreatePayload>;
 
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -7,20 +7,60 @@ import { ENUMS } from "@/constants/enum";
 
 const schema = new Schema<IUser, UserModel, IUserMethods>(
   {
+    name: { type: String, required: true },
     email: {
       type: String,
       unique: true,
       required: [true, "Email is required"],
       match: [REGEX.EMAIL, "Invalid email"],
+      maxlength: [255, "Email is too long"],
       index: true,
       lowercase: true,
     },
-    password: { type: String, required: [true, "Password is required"] },
-    name: { type: String, required: true },
-    emailVerified: { type: Boolean, default: false },
-    emailVerifiedAt: Date,
-    passwordResetAt: Date,
+    password: { type: String, required: [true, "Password is required"], select: false },
     role: { type: String, enum: ENUMS.USER_TYPES, default: "user" },
+    dob: {
+      type: Date,
+    },
+    mobileNumber: { type: String },
+    countryCode: { type: String },
+    countryCodeName: { type: String },
+    profilePicture: { type: String },
+    address: { type: String },
+    status: {
+      type: String,
+      enum: ENUMS.USER_STATUS,
+      default: "active",
+    },
+    notificationStatus: {
+      type: String,
+      enum: ENUMS.NOTIFICATION_STATUS,
+    },
+    locationStatus: {
+      type: String,
+      enum: ENUMS.LOCATION_STATUS,
+    },
+    latitude: { type: Number, select: false },
+    longitude: { type: Number, select: false },
+    otp: { type: String, select: false },
+    otpExpiresAt: { type: Date, select: false },
+    verified: { type: Boolean, default: false },
+    verifiedAt: Date,
+    hasTwoFactorAuth: { type: Boolean, default: false },
+    bio: { type: String },
+    loginWithQRCode: { type: Boolean, default: false },
+    loginQRCode: { type: String },
+    profileQrCode: { type: String },
+    // TODO: Add Ref to Plan Model
+    planId: { type: Schema.Types.ObjectId },
+    isPlanActive: { type: Boolean, default: false },
+    planPurchasedAt: { type: Date },
+    planExpiredAt: { type: Date },
+    emailVerified: { type: Boolean, default: false },
+    emailVerifiedAt: { type: Date, select: false },
+    passwordResetAt: { type: Date, select: false },
+    deviceType: { type: String, enum: ENUMS.DEVICE_TYPES },
+    deviceUniqueId: String,
   },
   { timestamps: true }
 );
@@ -38,8 +78,6 @@ schema.methods.toJSON = function () {
 
   obj.id = obj._id;
   delete obj.password;
-  delete obj.verifications;
-  delete obj.resetPasswords;
 
   return obj;
 };
