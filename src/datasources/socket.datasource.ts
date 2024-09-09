680d45cf080e5a5fde079b4f05df3234e6b65357
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2024-09-09 18:31:38 +0500

    feat: Add socket.io functionality for real-time communication and user authentication
    This commit adds socket.io functionality to enable real-time communication between clients and the server. It also includes user authentication using JWT tokens for secure communication.

--- src/datasources/socket.datasource.ts
+++ src/datasources/socket.datasource.ts
@@ -1,6 +1,13 @@
+import { authGuard } from "@/guards";
+import { authMiddleware } from "@/middlewares";
+import { getAccessTokenFromHeaders } from "@/utils/headers.util";
+import { jwtVerify } from "@/utils/jwt.util";
 import { Server as HttpServer } from "http";
 import { Server, Socket } from "socket.io";
 
+interface SocketWithContext extends Socket {
+  user?: { id: string };
+}
 export const socket = {
   run: async (server: HttpServer) => {
     const io = new Server(server, {
@@ -9,15 +16,41 @@ export const socket = {
       },
     });
 
-    const connections = new Map<string, Socket>();
+    const connections = new Map<string, string>();
 
-    io.on("connection", (socket: Socket) => {
-      connections.set(socket.id, socket);
+    io.use((socket: SocketWithContext, next) => {
+      const { accessToken } = getAccessTokenFromHeaders(socket.handshake.headers);
+      if (!accessToken) {
+        return next(new Error("Authentication error"));
+      }
 
-      socket.on("message", (message: string) => {
-        console.log(`Message: ${message}`);
-        // socket.broadcast.emit("message", message);
-        io.emit("message", message);
+      try {
+        const user = jwtVerify({ accessToken: accessToken });
+        Object.assign(socket, { user: { id: user.id.toString() } });
+        next();
+      } catch (error) {
+        next(new Error("Authentication error"));
+      }
+    });
+
+    io.on("connection", (socket: SocketWithContext) => {
+      if (!socket.user) {
+        io.to(socket.id).emit("error", "Authentication error");
+        return socket.disconnect();
+      }
+
+      connections.set(socket.user.id, socket.id);
+
+      socket.on("message", (message: string, receiverId: string) => {
+        if (!receiverId) {
+          return io.to(socket.id).emit("error", "Receiver not found");
+        }
+        const receiverSocketId = connections.get(receiverId);
+        if (!receiverSocketId) {
+          return io.to(socket.id).emit("error", "Receiver not found");
+        }
+
+        io.to(receiverSocketId).emit("message", message);
       });
 
       socket.on("disconnect", () => {
