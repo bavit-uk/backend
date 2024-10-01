978e6cc10fab72e96158fdcda842618166378464
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2024-10-01 13:51:10 +0500

    refactor: Update message.service.ts to include scannedBy field in message creation and update

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -3,13 +3,17 @@ import { Message } from "@/models";
 import { ClientSession } from "mongoose";
 
 export const messageService = {
-  create: ({ content, conversation, files, sender, isQrCode }: CreateMessagePayload, session?: ClientSession) =>
+  create: (
+    { content, conversation, files, sender, isQrCode, scannedBy }: CreateMessagePayload,
+    session?: ClientSession
+  ) =>
     new Message({
       content,
       conversation,
       files,
       sender,
       isQrCode,
+      scannedBy,
     }).save({ session }),
 
   getMessageById: (messageId: string) => Message.findById(messageId),
@@ -38,19 +42,32 @@ export const messageService = {
     );
   },
 
-  getAll: ({ conversation }: Pick<GetMessagePayload, "conversation">) => Message.find({ conversation }),
+  getAll: ({
+    conversation,
+    lastMessageDate = Date.now(),
+  }: Pick<GetMessagePayload, "conversation"> & { lastMessageDate: Number }) =>
+    Message.find({ conversation, createdAt: { $lte: lastMessageDate } }),
 
   getByConversationId: ({ conversation }: Pick<GetMessagePayload, "conversation">) => Message.find({ conversation }),
 
-  getLastMessage: ({ conversation }: Pick<GetMessagePayload, "conversation">) =>
-    Message.findOne({ conversation }).sort({ createdAt: -1 }).select("content createdAt files isQrCode"),
+  getLastMessage: ({
+    conversation,
+    lastMessageDate = Date.now(),
+  }: Pick<GetMessagePayload, "conversation"> & { lastMessageDate?: Number }) =>
+    Message.findOne({ conversation, createdAt: { $lte: lastMessageDate } })
+      .sort({ createdAt: -1 })
+      .select("content createdAt files isQrCode scannedBy"),
 
-  getTotalAndUnreadMessages: async (conversationId: string, to: string) => {
-    const totalMessagesPromise = Message.countDocuments({ conversation: conversationId });
+  getTotalAndUnreadMessages: async (conversationId: string, to: string, lastMessageDate: Number = Date.now()) => {
+    const totalMessagesPromise = Message.countDocuments({
+      conversation: conversationId,
+      createdAt: { $lt: lastMessageDate },
+    });
     const unreadMessagesPromise = Message.countDocuments({
       conversation: conversationId,
       read: false,
       sender: { $ne: to },
+      createdAt: { $lt: lastMessageDate },
     });
 
     const [totalMessages, unreadMessages] = await Promise.all([totalMessagesPromise, unreadMessagesPromise]);
@@ -58,7 +75,36 @@ export const messageService = {
     return { totalMessages, unreadMessages };
   },
 
-  readMessages: async ({ conversationId, to }: { conversationId: string; to: string }) => {
-    return Message.updateMany({ conversation: conversationId, sender: { $ne: to } }, { read: true });
+  readMessages: async ({
+    conversationId,
+    to,
+    lastMessageDate,
+  }: {
+    conversationId: string;
+    to: string;
+    lastMessageDate: Number;
+  }) => {
+    return Message.updateMany(
+      { conversation: conversationId, sender: { $ne: to }, createdAt: { $lt: lastMessageDate } },
+      { read: true }
+    );
+  },
+
+  findUnscannedMessages: async ({
+    conversation,
+    scannedBy,
+  }: Pick<UpdateMessagePayload, "conversation" | "scannedBy">) => {
+    const personToCheck = scannedBy;
+    return Message.find({ conversation, isQrCode: true, scannedBy: { $nin: personToCheck } })
+      .sort({ createdAt: 1 })
+      .limit(1);
+  },
+
+  addScannedBy: async ({
+    conversation,
+    id,
+    scannedBy,
+  }: Pick<UpdateMessagePayload, "conversation" | "id" | "scannedBy">) => {
+    return Message.findOneAndUpdate({ conversation, _id: id, isQrCode: true }, { $addToSet: { scannedBy } });
   },
 };
