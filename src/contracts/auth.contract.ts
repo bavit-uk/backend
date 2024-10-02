bcc68cc8455ab157b54357e6f9b4d1f7cac106c2
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2024-10-02 15:56:05 +0500

    feat: Add SignInWithQRPayload type to auth.contract.ts

--- src/contracts/auth.contract.ts
+++ src/contracts/auth.contract.ts
@@ -1,6 +1,7 @@
 import { IUser } from "./user.contract";
 
 export type SignInPayload = Pick<IUser, "email" | "password" | "otp" | "deviceType" | "deviceUniqueId">;
+export type SignInWithQRPayload = Pick<IUser, "loginQRCode">;
 
 export type SignUpPayload = Pick<IUser, "email" | "password" | "name"> & { role?: IUser["role"] };
 
