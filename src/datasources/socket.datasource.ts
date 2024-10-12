4bab5ce3b004d4a8ba8b6a1a077b1687a577aba8
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2024-10-12 16:10:27 +0500

    refactor: Update SocketManager to include conversation title in new message notification

--- src/datasources/socket.datasource.ts
+++ src/datasources/socket.datasource.ts
@@ -282,7 +282,7 @@ class SocketManager {
               body: string;
               imageUrl?: string;
             } = {
-              title: "New message from " + sender?.name + " in group" + conversation.title,
+              title: "New message from " + sender?.name + " in " + conversation.title,
               body: lockChat ? "Locked message" : message || "Attachment",
               imageUrl: files?.[0],
             };
