873e9112443f94d17bf78039c012ebbeed4720f9
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2024-10-24 21:45:56 +0500

    refactor: Add headers field to request contracts

--- src/contracts/request.contract.ts
+++ src/contracts/request.contract.ts
@@ -6,27 +6,34 @@ import { IUser } from "./user.contract";
 
 export interface IContextRequest<T> extends Omit<Request, "context"> {
   context: T;
+  headers: Request["headers"];
 }
 
 export interface IBodyRequest<T> extends Omit<Request, "body"> {
   body: T;
+  headers: Request["headers"];
 }
 
 export interface IParamsRequest<T> extends Request {
   params: T & ParamsDictionary;
+  headers: Request["headers"];
 }
 
 export interface IQueryRequest<T> extends Request {
   query: T & ParamsDictionary;
+  headers: Request["headers"];
 }
 
 export interface ICombinedRequest<Context, Body, Params = Record<string, unknown>, Query = Record<string, unknown>>
-  extends Pick<IContextRequest<Context>, "context">,
+  extends Pick<IContextRequest<Context>, "context" | "headers">,
     Pick<IBodyRequest<Body>, "body">,
     Pick<IParamsRequest<Params>, "params">,
-    Pick<IQueryRequest<Query>, "query"> {}
+    Pick<IQueryRequest<Query>, "query"> {
+  headers: Request["headers"];
+}
 
 export interface IUserRequest {
   user: Omit<IUser, "id"> & Document;
   accessToken: string;
+  headers: Request["headers"];
 }
