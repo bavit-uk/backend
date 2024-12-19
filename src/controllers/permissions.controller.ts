0f3f43e49fea84c0057474b00908638d1f72bd5a
Owais Mehmood <20-arid-815@student.uaar.edu.pk>
2024-12-19 20:03:28 +0500

    chores

--- src/controllers/permissions.controller.ts
+++ src/controllers/permissions.controller.ts
@@ -11,19 +11,38 @@ export const permissionsController = {
         },
         userManagement: {
           parent: "MANAGE_USERS",
-          children: ["ADD_USERS_CATEGORY", "VIEW_USERS_CATEGORY", "ADD_USERS", "VIEW_USERS"],
+          children: [
+            "ADD_USERS_CATEGORY",
+            "VIEW_USERS_CATEGORY",
+            "ADD_USERS",
+            "VIEW_USERS",
+          ],
         },
         supplierManagement: {
           parent: "MANAGE_SUPPLIERS",
-          children: ["ADD_SUPPLIERS_CATEGORY", "VIEW_SUPPLIERS_CATEGORY", "ADD_SUPPLIERS", "VIEW_SUPPLIERS"],
+          children: [
+            "ADD_SUPPLIERS_CATEGORY",
+            "VIEW_SUPPLIERS_CATEGORY",
+            "ADD_SUPPLIERS",
+            "VIEW_SUPPLIERS",
+          ],
         },
         inventoryManagement: {
           parent: "MANAGE_INVENTORY",
-          children: ["ADD_INVENTORY_CATEGORY", "VIEW_INVENTORY_CATEGORY", "ADD_INVENTORY", "VIEW_INVENTORY"],
+          children: [
+            "ADD_INVENTORY_CATEGORY",
+            "VIEW_INVENTORY_CATEGORY",
+            "ADD_INVENTORY",
+            "VIEW_INVENTORY",
+          ],
         },
         gamersCommunity: {
           parent: "GAMERS_COMMUNITY",
-          children: ["VIEW_GAMERS_COMMUNITY" , "VIEW_BLOGS"]
+          children: ["VIEW_GAMERS_COMMUNITY", "VIEW_BLOGS"],
+        },
+        policies: {
+          parent: "MANAGE_POLICIES",
+          children: ["VIEW_POLICIES", "VIEW_SUBSCRIPTIONS", "VIEW_FAQS"],
         },
         setting: {
           parent: "SETTINGS",
