c95443ad8699773bc50e77acf656ce3e4ba3d52a
Owais Mehmood <20-arid-815@student.uaar.edu.pk>
2025-01-01 15:24:54 +0500

    chores.

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -2,21 +2,33 @@ import { productController } from "@/controllers";
 import { productValidation } from "@/validations";
 import { Router } from "express";
 
-
 export const product = (router: Router) => {
+  // TODO: productValidation.addProduct
+  // Create or update a draft product
+  router.post("/", productController.createDraftProduct);
 
-    // TODO: productValidation.addProduct
-    router.post("/" , productController.createDraftProduct, productController.updateDraftProduct);
+  // Update a draft product by ID (subsequent steps)
+  router.patch(
+    "/:id",
+    productValidation.updateProduct,
+    productController.updateDraftProduct
+  );
+  router.get("/", productController.getAllProduct);
 
-    router.get("/", productController.getAllProduct);
+  router.get(
+    "/:id",
+    productValidation.validateId,
+    productController.getProductById
+  );
 
-    router.get("/:id" , productValidation.validateId , productController.getProductById);
+  // router.delete("/:id" , productValidation.validateId , productController.deleteProduct)
 
-    // router.delete("/:id" , productValidation.validateId , productController.deleteProduct)
+  router.patch(
+    "/:id",
+    productValidation.updateProduct,
+    productController.updateProductById
+  );
 
-    router.patch("/:id" , productValidation.updateProduct , productController.updateProductById)
-
-    // route for toggle block status
-    router.patch("/block/:id" , productController.toggleBlock)
-
-}
+  // route for toggle block status
+  router.patch("/block/:id", productController.toggleBlock);
+};
