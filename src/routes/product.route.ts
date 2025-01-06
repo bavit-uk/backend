1792db64b378a92776b28c3709807d59e8d3a0d7
Hammad Zamir <annibaba009@gmail.com>
2025-01-06 18:17:13 +0500

    get product by id

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -6,6 +6,7 @@ export const product = (router: Router) => {
   // TODO: productValidation.addProduct
   // Create or update a draft product
   router.post("/", productController.createDraftProduct);
+  
   router.get(
     "/transform/:id",
     productValidation.validateId,
