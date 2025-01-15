b0b0100d71f44865eccd08165ef22f7e27d76d4b
Ali-Babar123 <m.alibabar2004@gmail.com>
2025-01-15 18:54:21 +0500

    fix issues in validation

--- src/routes/faqs-policy.route.ts
+++ src/routes/faqs-policy.route.ts
@@ -1,15 +1,15 @@
 import { Router } from "express"
 import { faqsPolicyController } from "@/controllers"
-
+import { faqsValidation } from "@/validations";
 
 export const faqsPolicy = (router: Router) => {
 
-    router.post('/', faqsPolicyController.createFaqsPolicy);
+    router.post('/', faqsValidation.addFaqs, faqsPolicyController.createFaqsPolicy);
 
-    router.get('/', faqsPolicyController.getAllFaqsPolicy);
+    router.get('/',  faqsPolicyController.getAllFaqsPolicy);
 
-    router.delete('/:id', faqsPolicyController.deleteFaqs);
+    router.delete('/:id', faqsValidation.validateId, faqsPolicyController.deleteFaqs);
 
-    router.patch('/:id', faqsPolicyController.editFaqs)
+    router.patch('/:id',  faqsValidation.editFaqs, faqsPolicyController.editFaqs)
 
 }
