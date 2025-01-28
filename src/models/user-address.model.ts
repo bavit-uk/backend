695c0b5c7deddbe7193c2de21911d4746dec47fb
owais815 <98080924+owais815@users.noreply.github.com>
2025-01-28 15:38:12 +0500

    refactor: nodemon configured

--- src/models/user-address.model.ts
+++ src/models/user-address.model.ts
@@ -1,21 +1,24 @@
-import mongoose, { Schema, model , Document } from "mongoose";
-import { IUserAddress , UserAddressModel} from "@/contracts/user-address.contracts";
+import mongoose, { Schema, model, Document } from "mongoose";
 
-const AddressSchema = new Schema<IUserAddress , UserAddressModel>(
-    {
-      userId: { type: Schema.Types.ObjectId, ref: "User"},
-      country: { type: String},
-      address: { type: String},
-      label: { type: String},
-      appartment: { type: String},
-      city: { type: String },
-      postalCode: { type: String},
-      isDefault: { type: Boolean},
-    },
-    {
-      timestamps: true,
-    }
+import {
+  IUserAddress,
+  UserAddressModel,
+} from "@/contracts/user-address.contracts";
+
+const AddressSchema = new Schema<IUserAddress, UserAddressModel>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+    country: { type: String },
+    address: { type: String },
+    label: { type: String },
+    appartment: { type: String },
+    city: { type: String },
+    postalCode: { type: String },
+    isDefault: { type: Boolean },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-export const Address = model('Address', AddressSchema);
-  
+export const Address = model("Address", AddressSchema);
