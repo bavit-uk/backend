f75c38ea81bc2fac1e90a6502b420ff6991dcb11
owais815 <98080924+owais815@users.noreply.github.com>
2025-01-29 15:24:15 +0500

    signup phone number optional now

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 import { StatusCodes } from "http-status-codes";
 
 export const corsMiddleware = cors({
-  origin: ["https://bavit-test.vercel.app", "http://localhost:3000","http://localhost:3001", "https://bavit-dev.vercel.app"], // Allows requests from any origin
+  origin: ["https://bavit-test.vercel.app", "http://localhost:3000","http://localhost:3001", "https://bavit-dev.vercel.app", "https://512l06gl-3000.inc1.devtunnels.ms"], // Allows requests from any origin
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow these methods
   allowedHeaders: ["Content-Type", "Authorization", "x-user-type"], // Allow these headers
   credentials: true, // Allows sending cookies if needed
