b625b7291166225e884cfd52d165b664aff92a78
owais815 <98080924+owais815@users.noreply.github.com>
2025-01-29 18:10:32 +0500

    refactor: cors again

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -4,8 +4,8 @@ import { StatusCodes } from "http-status-codes";
 export const corsMiddleware = cors({
   // origin: ["https://bavit-test.vercel.app", "http://localhost:3000","http://localhost:3001", "https://bavit-dev.vercel.app", "https://512l06gl-3000.inc1.devtunnels.ms"], // Allows requests from any origin
   origin: "*", // Allows requests from any origin
-  methods: ["GET", "POST","PATCH", "PUT", "DELETE", "OPTIONS"], // Allow these methods
-  allowedHeaders: ["Content-Type", "Authorization", "x-user-type"], // Allow these headers
-  credentials: true, // Allows sending cookies if needed
+  // methods: ["GET", "POST","PATCH", "PUT", "DELETE", "OPTIONS"], // Allow these methods
+  // allowedHeaders: ["Content-Type", "Authorization", "x-user-type"], // Allow these headers
+  // credentials: true, // Allows sending cookies if needed
   optionsSuccessStatus: StatusCodes.OK, // Ensure proper status code
 });
