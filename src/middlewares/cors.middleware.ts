7e405c52e06ab1b5d2f80121f3a544055217b348
owais815 <98080924+owais815@users.noreply.github.com>
2025-01-29 13:06:44 +0500

    refactor: added header

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -4,7 +4,7 @@ import { StatusCodes } from "http-status-codes";
 export const corsMiddleware = cors({
   origin: "*", // Allows requests from any origin
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow these methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allow these headers
+  allowedHeaders: ["Content-Type", "Authorization", "x-user-type"], // Allow these headers
   credentials: true, // Allows sending cookies if needed
   optionsSuccessStatus: StatusCodes.OK, // Ensure proper status code
 });
