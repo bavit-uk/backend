01c85f75a39ad775a11517ff8c2a1a070c2b41e3
owais815 <98080924+owais815@users.noreply.github.com>
2025-02-12 21:47:00 +0500

    add ICoupon interface for coupon management in mongoose

---
+++ src/contracts/coupon.contract.ts
@@ -0,0 +1,18 @@
+import { Document } from "mongoose";
+
+// Full Cart Interface
+interface ICoupon extends Document {
+  code: string;
+  discountType: "percentage" | "fixed"; // Percentage discount or fixed amount discount
+  discountValue: number;
+  maxDiscount?: number; // Optional max discount cap for percentage-based coupons
+  minPurchaseAmount?: number; // Minimum order value required to use the coupon
+  expiryDate: Date;
+  isActive: boolean;
+  usageLimit: number;
+  usageCount: number;
+  applicableProducts?: string[]; // Array of product IDs
+  applicableCategories?: string[]; // Array of category IDs
+}
+
+export { ICoupon };
