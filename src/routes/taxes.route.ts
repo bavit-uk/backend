1abf6fa01a84d7b35159c3ae85429446a727d64e
owais815 <98080924+owais815@users.noreply.github.com>
2025-02-12 21:46:15 +0500

    add taxes routes for CRUD operations

---
+++ src/routes/taxes.route.ts
@@ -0,0 +1,15 @@
+import { taxesController } from "@/controllers"; // Assuming you have a cart controller
+import { authGuard } from "@/guards"; // Importing authentication guard
+import { Router } from "express"; // Express Router for routing
+import { auth } from "firebase-admin"; // Firebase authentication
+
+export const taxesRoutes = (router: Router) => {
+  // Optionally, add an authentication guard if needed
+  // router.use(authGuard.isAuth);
+
+  router.post("/", taxesController.createTax);
+  router.get("/", taxesController.getAllTaxes);
+  router.put("/", taxesController.updateTax);
+  router.delete("/:country/:state", taxesController.deleteTax);
+};
+
