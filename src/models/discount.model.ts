ca7c75ee6698cce3a953b260938e17bae48e499f
owais815 <98080924+owais815@users.noreply.github.com>
2025-02-12 23:37:30 +0500

    add discount model with schema for discount management

---
+++ src/models/discount.model.ts
@@ -0,0 +1,23 @@
+import { IDiscount } from "@/contracts/discount.contract";
+import mongoose, { Schema } from "mongoose";
+
+const DiscountSchema = new Schema<IDiscount>(
+  {
+    discountName: { type: String },
+    discountType: {
+      type: String,
+      enum: ["percentage", "fixed"],
+      required: true,
+    },
+    discountValue: { type: Number, required: true, default: 0 },
+    maxDiscount: { type: Number },
+    minPurchaseAmount: { type: Number, default: 0 },
+    applicableProducts: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+    applicableCategories: [
+      { type: Schema.Types.ObjectId, ref: "ProductCategory" },
+    ], // Store category IDs
+  },
+  { timestamps: true }
+);
+
+export const Discount = mongoose.model<IDiscount>("Discount", DiscountSchema);
