f446173c9e2d8e80fe1a96d5a58a9890bee528f3
owais815 <98080924+owais815@users.noreply.github.com>
2025-02-13 22:27:56 +0500

    feat(stock): export Stock model and stock service in index files

--- src/services/index.ts
+++ src/services/index.ts
@@ -12,3 +12,4 @@ export { cartService } from "./cart.service";
 export { paymentPolicyService } from "./payment-policy.service";
 export { postagePolicyService } from "./postage-policy.service";
 export { variationService } from "./variation.service";
+export { stockService } from "./stock.service";
