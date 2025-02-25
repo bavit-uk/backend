113f17e281de54c6e5e644e22d3e9850c2fe4105
owais815 <98080924+owais815@users.noreply.github.com>
2025-02-25 17:57:32 +0500

    add endpoint to retrieve unified parts in variations route

--- src/routes/variations.route.ts
+++ src/routes/variations.route.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { variationController } from "@/controllers/variation.controller";
 
 export const variations = (router: Router) => {
+  router.get("/unified-parts", variationController.getUnifiedParts);
   router.post("/", variationController.createVariation);
 
   router.get("/:productId", variationController.getVariationsByProduct);
@@ -15,5 +16,4 @@ export const variations = (router: Router) => {
 
   router.get("/", variationController.getAllVariationsByPlatform);
 
-  router.get("/unified-parts", variationController.getUnifiedParts);
 };
