dd35bcdcbcb15c5705b08cfa70847f6a1217a63f
owais815 <98080924+owais815@users.noreply.github.com>
2025-02-27 16:45:23 +0500

    add pre-save hook to automatically generate batch number for stock entries

--- src/models/stock.model.ts
+++ src/models/stock.model.ts
@@ -17,11 +17,32 @@ const StockSchema = new Schema<IStock>(
     purchasePricePerUnit: { type: Number, required: true, min: 0 },
     costPricePerUnit: { type: Number, required: true, min: 0 },
     retailPricePerUnit: { type: Number, required: true, min: 0 },
-    batchNumber: { type: Number, required: true, unique: true , min: 0 },
+    batchNumber: { type: Number, required: true, unique: true, min: 0 },
     receivedDate: { type: Date, required: true, default: Date.now },
     purchaseDate: { type: Date, default: Date.now },
   },
   { timestamps: true }
 );
+// Pre-save hook to generate batch number automatically
+StockSchema.pre<IStock>("save", async function (next) {
+  if (!this.batchNumber) {
+    try {
+      // Find the latest batchNumber
+      const lastStock = await mongoose
+        .model("Stock")
+        .findOne()
+        .sort({ batchNumber: -1 })
+        .exec();
+
+      // If there's no stock found, initialize batchNumber to 1
+      const newBatchNumber = lastStock ? lastStock.batchNumber + 1 : 1;
+
+      this.batchNumber = newBatchNumber;
+    } catch (error: any) {
+      return next(error);
+    }
+  }
+  next();
+});
 
 export const Stock = mongoose.model<IStock>("Stock", StockSchema);
