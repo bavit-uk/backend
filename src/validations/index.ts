7e0c122ff1c037298d6abf336d5b64de3e206907
Owais Mehmood <sultanowaismehmood@gmail.com>
2025-03-14 00:17:28 +0500

    refactor: remove transformProductData utility and update listing validation export

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -5,7 +5,7 @@ export { supplierValidation } from "./supplier.validation";
 export { supplierCategoryValidation } from "./supplier-category.validation";
 export { productCategoryValidation } from "./product-category.validation";
 export { productBrandValidation } from "./product-brand.validation";
-export { productValidation } from "./listing.validation";
+export { listingValidation } from "./listing.validation";
 export { inventoryValidation } from "./inventory.validation";
 export { orderValidation } from "./order.validation";
 export { cartValidation } from "./cart.validation";
