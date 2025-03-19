ef6494cce5161e7afdf4c9036864247204b6e1de
Owais Mehmood <sultanowaismehmood@gmail.com>
2025-03-20 00:21:46 +0500

    refactor: update inventory model schemas to support array types for multiple values

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,8 @@
-import { Inventory } from "@/models";
 export { User } from "./user.model";
 export { UserCategory } from "./user-category.model";
 export { Address } from "./user-address.model";
 export { SupplierCategory } from "./supplier-category.model";
 export { ProductCategory } from "./product-category.model";
-
 export { ProductBrand } from "./product-brand.model";
 export { Listing } from "./listing.model";
 export { PaymentPolicy } from "./payment-policy.model";
