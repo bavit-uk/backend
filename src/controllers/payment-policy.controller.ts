29cf106cc7cb56ce9d076eca710f6d9783487962
Owais Mehmood <sultanowaismehmood@gmail.com>
2025-04-11 22:00:22 +0500

    refactor: remove toggleBlock method from payment policy controller and route

--- src/controllers/payment-policy.controller.ts
+++ src/controllers/payment-policy.controller.ts
@@ -1,4 +1,4 @@
-import { paymentPolicyService, ebayPaymentPolicyService } from "@/services";
+import {  ebayPaymentPolicyService } from "@/services";
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
@@ -109,19 +109,5 @@ export const paymentPolicyController = {
       });
     }
   },
-  toggleBlock: async (req: Request, res: Response) => {
-    try {
-      const { id } = req.params;
-      const { isBlocked } = req.body;
-      const result = await paymentPolicyService.toggleBlock(id, isBlocked);
-      res.status(StatusCodes.OK).json({
-        success: true,
-        message: `Policy ${isBlocked ? "blocked" : "unblocked"} successfully`,
-        data: result,
-      });
-    } catch (error) {
-      console.error("Toggle Block Policy Error:", error);
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ success: false, message: "Error updating policy status" });
-    }
-  },
+  
 };
