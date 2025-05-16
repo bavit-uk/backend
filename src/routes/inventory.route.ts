bd2230cbdc0576089a16f14b014eb5afbb21bcda
Owais Mehmood <sultanowaismehmood@gmail.com>
2025-05-16 19:31:51 +0500

    fix: comment out fetch-all-categories route for bulk import template creation

--- src/routes/inventory.route.ts
+++ src/routes/inventory.route.ts
@@ -65,7 +65,7 @@ export const inventory = (router: Router) => {
   router.post("/:id/generate-variations", inventoryController.generateAndStoreVariations);
 
   router.patch("/:id/update-variations", inventoryController.storeSelectedVariations);
-  router.get("/fetch-all-categories", bulkImportUtility.fetchAspectsForAllCategories);
+  // router.get("/fetch-all-categories", bulkImportUtility.fetchAspectsForAllCategories); only usable route to get all gategories from ebay and to create bulk import template
   router.get("/:id", inventoryValidation.validateId, inventoryController.getInventoryById);
   // Get selected variations for a inventory
   router.get("/:id/selected-parts", inventoryController.getSelectedInventoryParts);
