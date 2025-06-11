17717591bdf82acdc9bfc2a17ddae28064db323f
Owais Mehmood <sultanowaismehmood@gmail.com>
2025-06-11 21:58:51 +0500

    Update access token in amazon_tokens_sandbox.json; add get-item-from-amazon route to amazon.route.ts

--- src/routes/amazon.route.ts
+++ src/routes/amazon.route.ts
@@ -21,7 +21,9 @@ export const amazon = (router: Router) => {
   router.get("/get-parsed-schema/:productType", amazonListingService.getAmazonSchemaDummy);
   router.get("/get-original-schema/:productType", amazonListingService.getAmazonSchemaOriginal);
   router.get("/check-amazon-listing-status/:sku", listingController.checkAmazonListingStatus);
+  router.get("/get-item-from-amazon/:listingId", listingController.getItemFromAmazon);
   // router.get("/check-amazon-submission-status/:submissionId", listingController.checkAmazonSubmissionStatus);
+
 
   // Additional routes can be added here as needed
   // For example:
