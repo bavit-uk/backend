575dbfdb253df6c89ed9d27fd7609181792ff0da
Abdulrehman Abbasi <abdulrehmanabbasi5067794@gmail.com>
2025-07-14 19:09:49 +0500

    Added the isBlocked field to gamer-community model , update contract routes service and controller

--- src/controllers/gamer-comunity.controller.ts
+++ src/controllers/gamer-comunity.controller.ts
@@ -5,16 +5,16 @@ import { StatusCodes, ReasonPhrases } from "http-status-codes";
 export const gamercomunityController = {
   addgamercomunity: async (req: Request, res: Response) => {
     try {
-      const { title, description, image, privacy } = req.body;
-      console.log(title, description, image, privacy);
-      const newgamercomunity = await GamerComunityService.creategamercomunity(title, description, image, privacy);
+      const { title, description, image, privacy, isBlocked } = req.body;
+      console.log(title, description, image, privacy, isBlocked);
+      const newgamercomunity = await GamerComunityService.creategamercomunity(title, description, image, privacy, isBlocked);
       res
         .status(StatusCodes.CREATED)
         .json({ success: true, message: "gamercomunity gamercomunity created successfully", data: newgamercomunity });
     } catch (error: any) {
       console.error(error);
       if (error.title === "MongoServerError" && error.code === 11000) {
-        const field = Object.keys(error.keyPattern)[0]; 
+        const field = Object.keys(error.keyPattern)[0];
         res.status(StatusCodes.BAD_REQUEST).json({
           message: `The ${field} must be unique. "${req.body[field]}" is already in use.`,
         });
@@ -28,8 +28,8 @@ export const gamercomunityController = {
   editgamercomunity: async (req: Request, res: Response) => {
     try {
       const { id } = req.params;
-      const { title, description, image, privacy } = req.body;
-      const gamercomunity = await GamerComunityService.editgamercomunity(id, { title, description, image , privacy });
+      const { title, description, image, privacy, isBlocked } = req.body;
+      const gamercomunity = await GamerComunityService.editgamercomunity(id, { title, description, image, privacy, isBlocked });
       res.status(StatusCodes.OK).json({ success: true, message: "gamercomunity updated successfully", data: gamercomunity });
     } catch (error: any) {
       if (error.title === "MongoServerError" && error.code === 11000) {
@@ -86,5 +86,5 @@ export const gamercomunityController = {
     }
   },
 
- 
+
 };
--- src/models/gamer-comunity.model.ts
+++ src/models/gamer-comunity.model.ts
@@ -1,26 +1,30 @@
-import mongoose, {Schema, model} from "mongoose";
-import { Gamercomunity, GamercomunityModel, PrivacyStatus  } from "@/contracts/gamer-comunity.contract";
+import mongoose, { Schema, model } from "mongoose";
+import { Gamercomunity, GamercomunityModel, PrivacyStatus } from "@/contracts/gamer-comunity.contract";
 
 
-    const gamerComunity = new Schema<Gamercomunity, GamercomunityModel>({
-        title: { 
-            type: String, 
-            required: [true, 'Title is required'],
-          },
-          description: { 
-            type: String, 
-            required: [true, 'Description is required'],
-          },
-          image: { 
-            type: String, 
-            required: [true, 'Image URL is required'],
-          },
-          privacy: { 
-            type: String, 
-            enum: Object.values(PrivacyStatus),
-            default: PrivacyStatus.PUBLIC,
-            required: true
-          }
-    })
+const gamerComunity = new Schema<Gamercomunity, GamercomunityModel>({
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Description is required'],
+  },
+  image: {
+    type: String,
+    required: [true, 'Image URL is required'],
+  },
+  privacy: {
+    type: String,
+    enum: Object.values(PrivacyStatus),
+    default: PrivacyStatus.PUBLIC,
+    required: true
+  },
+  isBlocked: {
+    type: Boolean,
+    default: false,
+  }
+})
 
-    export const GamerComunity = model("gamercomunity", gamerComunity)
+export const GamerComunity = model("gamercomunity", gamerComunity)
