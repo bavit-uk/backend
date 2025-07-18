a8d96047b2407e4213b86847c3b6fe68905dbdf1
Hasaan Ahmad <hasaanahmad10023@gmail.com>
2025-07-18 15:07:12 +0500

    Update cron schedules to run every 2 hours for auto-checkout and marking absent jobs.

--- src/cron/attendance/autoCheckout.cron.ts
+++ src/cron/attendance/autoCheckout.cron.ts
@@ -2,7 +2,7 @@ import nodeCron from "node-cron";
 import { autoCheckoutForUsers } from "../../services/attendance.service";
 
 export const autoCheckoutCron = () => {
-  nodeCron.schedule("*/10 * * * * *", async () => {
+  nodeCron.schedule("* */2 * * * *", async () => {
     await autoCheckoutForUsers();
     console.log("Auto Checkout cron job executed");
   });
