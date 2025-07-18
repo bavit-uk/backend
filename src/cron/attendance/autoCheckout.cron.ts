47d6d263c844ff4ca2cc4eefb5daf7ff58e1db01
Hasaan Ahmad <hasaanahmad10023@gmail.com>
2025-07-18 15:46:42 +0500

    time configured

--- src/cron/attendance/autoCheckout.cron.ts
+++ src/cron/attendance/autoCheckout.cron.ts
@@ -2,7 +2,7 @@ import nodeCron from "node-cron";
 import { autoCheckoutForUsers } from "../../services/attendance.service";
 
 export const autoCheckoutCron = () => {
-  nodeCron.schedule("* */2 * * * *", async () => {
+  nodeCron.schedule("0 */2 * * *", async () => {
     await autoCheckoutForUsers();
     console.log("Auto Checkout cron job executed");
   });
--- src/cron/attendance/markAbsent.cron.ts
+++ src/cron/attendance/markAbsent.cron.ts
@@ -2,7 +2,7 @@ import nodeCron from "node-cron";
 import { markAbsentForUsers } from "../../services/attendance.service";
 
 export const markAbsentCron = () => {
-  nodeCron.schedule("* */2 * * * *", async () => {
+  nodeCron.schedule("0 */2 * * *", async () => {
     await markAbsentForUsers();
     console.log("Mark Absent cron job executed");
   });
