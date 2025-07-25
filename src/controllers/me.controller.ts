9ba3893ec5d7ca43820a68306e18ada75fade75e
Hasaan Ahmad <hasaanahmad10023@gmail.com>
2025-07-25 16:48:28 +0500

    changes

--- src/controllers/me.controller.ts
+++ src/controllers/me.controller.ts
@@ -2,12 +2,13 @@ import { Request, Response } from "express";
 import { StatusCodes, ReasonPhrases } from "http-status-codes";
 
 export const meController = {
-
   info: (req: Request, res: Response) => {
     try {
-      const user = req.context.user;
+      const user = req.context?.user;
       if (!user) {
-        return res.status(StatusCodes.NOT_FOUND).json({ message: "User not found or unAuthorized" });
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ message: "User not found or unAuthorized" });
       }
       res.status(StatusCodes.OK).json({
         success: true,
@@ -22,5 +23,4 @@ export const meController = {
       });
     }
   },
-
 };
