6322d03c9ef084949fea573334f08c554906c24c
Hasaan Ahmad <hasaanahmad10023@gmail.com>
2025-07-25 14:34:33 +0500

    new updated logic for Attendance

--- src/cron/attendance/markAbsent.cron.ts
+++ src/cron/attendance/markAbsent.cron.ts
@@ -1,8 +1,8 @@
 import nodeCron from "node-cron";
-import { markAbsentForUsers } from "../../services/attendance.service";
+import { markAbsentForUsers } from "@/services/attendance.service";
 
 export const markAbsentCron = () => {
-  nodeCron.schedule("0 */2 * * *", async () => {
+  nodeCron.schedule("0 0 */2 * * *", async () => {
     await markAbsentForUsers();
     console.log("Mark Absent cron job executed");
   });
