a4cc9cfd28396aba625fb2992c6c898e0cd4e264
Owais Mehmood <sultanowaismehmood@gmail.com>
2025-07-28 19:19:22 +0500

    Update JWT expiration times for access and refresh tokens

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -4,15 +4,12 @@ import { Types } from "mongoose";
 import { IAccessToken, IJwtUser } from "@/contracts/jwt.contract";
 
 export const jwtSign = (id: Types.ObjectId): IAccessToken => {
-  const accessToken:any = jwt.sign({ id }, process.env.JWT_SECRET || "", {
-    expiresIn: process.env.JWT_EXPIRATION || "1m",
+  const accessToken: any = jwt.sign({ id }, process.env.JWT_SECRET || "", {
+    expiresIn: process.env.JWT_EXPIRATION || "1d",
   });
 
-
-
-
   const refreshToken = jwt.sign({ id }, process.env.JWT_SECRET || "", {
-    expiresIn: process.env.JWT_REFRESH_EXPIRATION || "1m",
+    expiresIn: process.env.JWT_REFRESH_EXPIRATION || "7d",
   });
 
   return { accessToken, refreshToken };
