9a0492dc58d0a9591fc71332d65f1f859f261a5e
Owais Mehmood <sultanowaismehmood@gmail.com>
2025-08-15 14:40:33 +0500

    shifted routes to its specific folder

--- src/routes/website.route.ts
+++ src/routes/website.route.ts
@@ -1,9 +1,16 @@
-import { websiteController } from "@/controllers";
+import { listingController, websiteController } from "@/controllers";
+import { listingValidation } from "@/validations";
 import { Router } from "express";
 
 export const website = (router: Router) => {
   // Fetch featured categories for website
   router.get("/featured-categories", websiteController.getFeaturedCategoriesForWebsite);
 
-  
+  // website routes
+  // Fetch all Website listings
+  router.get("/website", listingController.getWebsiteListings);
+
+  // Fetch single Website product by ID
+  router.get("/website/:id", listingValidation.validateId, listingController.getWebsiteProductById);
+  router.patch("/isfeatured/:id", listingController.toggleIsFeatured);
 };
